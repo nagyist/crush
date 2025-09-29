f04c3df3dfa6094fc26d4b818c64598ff48ea05e
tauraamui <adampstringer@protonmail.com>
2025-09-29 16:35:23 +0100

    feat(chat): define crush context and pass into send message func

---
+++ internal/crush/context.go
@@ -0,0 +1,46 @@
+package crush
+
+import (
+	"github.com/charmbracelet/crush/internal/llm/agent"
+	"github.com/charmbracelet/crush/internal/session"
+	"github.com/charmbracelet/crush/internal/tui/components/chat"
+)
+
+type StorePromptMsg struct {
+	msg chat.Msg
+}
+
+type PromptHistory struct {
+	SessionID int
+}
+
+type Context interface {
+	ResolveCurrentSession() (session.Session, error)
+	CoderAgent() (agent.Service, bool)
+}
+
+func NewContext() Context {
+	return &context{}
+}
+
+type context struct {
+	sessions session.Service
+}
+
+func (c *context) ResolveCurrentSession() (session.Session, error) {
+	/*
+		if p.session.ID == "" {
+			newSession, err := p.app.Sessions.Create(context.Background(), "New Session")
+			if err != nil {
+				return util.ReportError(err)
+			}
+			session = newSession
+			cmds = append(cmds, util.CmdHandler(chat.SessionSelectedMsg(session)))
+		}
+	*/
+	return session.Session{}, nil
+}
+
+func (c *context) CoderAgent() (agent.Service, bool) {
+	return nil, false
+}
