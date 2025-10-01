e6af3cd09dede8c5190ef341d9ff262daafaeb55
tauraamui <adampstringer@protonmail.com>
2025-10-01 15:01:49 +0100

    test(chat): ensure agent run failure behaves as expected

--- internal/tui/page/chat/chat_test.go
+++ internal/tui/page/chat/chat_test.go
@@ -10,19 +10,21 @@ import (
 	"github.com/charmbracelet/crush/internal/session"
 	"github.com/charmbracelet/crush/internal/tui/components/chat"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 type mockAgentRunner struct {
 	idOfSessionRanWithin  string
 	ranWithMsgText        string
 	ranWithMsgAttachments []message.Attachment
+	errOnRun              error
 }
 
 func (a *mockAgentRunner) Run(ctx context.Context, sessionID string, content string, attachments ...message.Attachment) (<-chan agent.AgentEvent, error) {
 	a.idOfSessionRanWithin = sessionID
 	a.ranWithMsgText = content
 	a.ranWithMsgAttachments = attachments
-	return nil, nil
+	return nil, a.errOnRun
 }
 
 type mockCrushCtx struct {
@@ -54,10 +56,34 @@ func TestOnSendChatMessage_RunsAgentWithMessageData(t *testing.T) {
 		currentSession: currentSess,
 		runner:         &agentRunner,
 	}
-	onSendChatMessage(&cctx, chat.Msg{
+	cmds := onSendChatMessage(&cctx, chat.Msg{
 		Text: "Fake message for test",
 	})
 
+	require.Len(t, cmds, 1)
+	assert.IsType(t, chat.SessionSelectedMsg{}, cmds[0]())
+	assert.Equal(t, currentSess.ID, agentRunner.idOfSessionRanWithin)
+	assert.Equal(t, "Fake message for test", agentRunner.ranWithMsgText)
+	assert.Nil(t, agentRunner.ranWithMsgAttachments)
+}
+
+func TestOnSendChatMessage_RunsAgentWithMessageDataButAgentFailsToRun(t *testing.T) {
+	currentSess := session.Session{
+		ID: "test-chat-session",
+	}
+	agentRunner := mockAgentRunner{
+		errOnRun: errors.New("agent failed to run"),
+	}
+	cctx := mockCrushCtx{
+		currentSession: currentSess,
+		runner:         &agentRunner,
+	}
+	cmds := onSendChatMessage(&cctx, chat.Msg{
+		Text: "Fake message for test",
+	})
+
+	require.Len(t, cmds, 2)
+	assert.IsType(t, chat.SessionSelectedMsg{}, cmds[0]())
 	assert.Equal(t, currentSess.ID, agentRunner.idOfSessionRanWithin)
 	assert.Equal(t, "Fake message for test", agentRunner.ranWithMsgText)
 	assert.Nil(t, agentRunner.ranWithMsgAttachments)
